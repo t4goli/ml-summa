c512dd8d19a3d607e27e6bf319c345efc5996ca4
Trisha Goli <golitrish4@gmail.com>
2026-03-30 20:04:49 -0400

    model eval MSE

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
 
 df = pd.read_csv("student_exam_scores.csv", sep=",")
 
@@ -20,3 +22,13 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 print(y_pred[:5])
+
+results = pd.DataFrame({
+    "Actual": y_test,
+    "Predicted": y_pred
+})
+
+print(results.head(10))
+
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
