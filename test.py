d8e033f645fbf5721c09850c18c115ff644f5fed
Trisha Goli <golitrish4@gmail.com>
2026-03-30 20:37:35 -0400

    added decision tree model +compared perfs

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
 
 
 df = pd.read_csv("student_exam_scores.csv", sep=",")
@@ -23,10 +24,11 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 model.fit(X_train_scaled, y_train)
-
+tree_model = DecisionTreeRegressor(random_state=42)
+tree_model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test_scaled)
-print(y_pred[:5])
+y_pred_tree = tree_model.predict(X_test)
 
 results = pd.DataFrame({
     "Actual": y_test,
@@ -38,5 +40,5 @@ print(results.head(10))
 mse = mean_squared_error(y_test, y_pred)
 print("MSE:", mse)
 
-print(model.coef_)
-print(model.intercept_)
+mse_tree = mean_squared_error(y_test, y_pred_tree)
+print("Tree MSE:", mse_tree)
